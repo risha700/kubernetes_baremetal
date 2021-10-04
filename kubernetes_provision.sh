d144d6d22d17fa2c59026aa75f5aa8e1b3211da6
Ahmed Shehab <ahbox@outlook.com>
2021-10-05 00:15:35 +0400

    added calico option

--- kubernetes_provision.sh
+++ kubernetes_provision.sh
@@ -265,9 +265,11 @@ init
 
 echo -e " \e[44m  ALL GOOD \e[0m
   TO initialize a cluster plane NOW RUN:\n
-    \e[38;5;44m $ sudo kubeadm init --pod-network-cidr=10.244.0.0/16 --ignore-preflight-errors=all --v=5 --cri-socket /var/run/dockershim.sock \e[0m \n
+    \e[38;5;44m $ sudo kubeadm init --pod-network-cidr=10.17.0.0/16 --ignore-preflight-errors=all --v=5 --cri-socket /var/run/dockershim.sock \e[0m \n
   THEN INSTALL NETWROKING FLANNEL:\n
     \e[38;5;44m  $ sudo kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml \e[0m  \n
+  OR CALICO:\n
+    \e[38;5;44m  $ sudo kubectl apply -f https://docs.projectcalico.org/v3.14/manifests/calico.yaml \e[0m  \n
   IF YOU WANT TO JOIN YOUR WORKER NODES:\n
    \e[38;5;44m  $ kubeadm join --discovery-token .... --discovery-token-ca-cert-hash ... \e[0m 
 "
